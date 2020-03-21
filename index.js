72305789c5cdb0e38e41e7ba3c0173aa6dfe7979
Nathan Grove <nathan@nathangrove.com>
2020-03-21 08:41:57 -0500

    first POC

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
+const crypto = require('crypto')
 
 
-var database = {
-  "sessions": {},
-  "users": {}
-};
+
+var database;
+var sockets = [];
 
 function logIt(req,res){
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
@@ -17,41 +17,93 @@ function logIt(req,res){
 app.get('/', function(req, res){
   return res.sendFile(__dirname + '/index.html');
 });
+app.get('/admin', function(req, res){
+  return res.sendFile(__dirname + '/admin.html');
+});
 
 app.use('*', (req,res,next) => {
-  let authorization = req.headers['authorization'];
+
+  if (!req.headers['authorization']){
+    res.sendStatus(401);
+    return logIt(req,res);
+  }
+
+  let authorization = req.headers['authorization'].split(' ')[1];
   if (!authorization){
     res.sendStatus(401);
     return logIt(req,res);
   }
 
   let [username, password] = Buffer.from(authorization, 'base64').toString().split(':');
-  console.log(username,password);
+
+  let pwd = crypto.createHash('md5').update(password).digest("hex")
+
+  let user = database.users.find( u => u.username == username );
+
+  if ( user && user.password == pwd ){
+    req.username = username;
+    next();
+
+  } else {
+    res.sendStatus(401);
+    return logIt(req,res);
+  }
 })
 
 app.get('/api/sessions', (req,res,next) => {
 
 
-  res.send({ sessions: database.sessions });
+  res.send({ result: database.sessions.filter( s => s.username && ( s.status == 'IN-MEETING' || s.status == 'CONNECTED' ) ) });
 
   next();
     
 });
 
+app.get('/api/sessions/:session/own/:id', (req,res,next) => {
+
+  let session = database.sessions.find( s => s.id == req.params.session );
+  if (!session){
+    res.status(400);
+    res.send({ error: "Invalid session ID" });
+
+  } else if (session.status == 'DISCONNECTED'){
+      res.status(400);
+      res.send({ error: "Session already disconnected" });
+
+  } else if (session.status == 'IN-MEETING'){
+    res.status(400);
+    res.send({ error: "Session is already in a meeting" });
+  } else res.send({ success: true });
+
+  session.owner = req.username;
+  session.status = 'IN-MEETING';
+  console.log(session);
+
+  var socket = sockets.find( s => s.session.id == session.id );
+  socket.sock.emit('join',req.params.id);
+
+  next();
+
+});
+
 
 app.use('*', (req,res) => {
   logIt(req,res);
+  res.end();
 });
 
 
 io.on('connection', function(socket){
 
+  let session = {};
+
   var establishSession = function(){
     var id = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
     socket.emit('id', id);
-    database.sessions[id] = {};
-    database.sessions[id].id = id;
-    database.sessions[id].created = new Date().toISOString();
+    session.id = id;
+    session.created = new Date().getTime();
+    session.lastConnected = new Date().getTime();
+    database.sessions.push(session);
   }
   
   socket.on('new', () => {
@@ -59,31 +111,57 @@ io.on('connection', function(socket){
   });
   
   socket.on('id', (id) => {
-    console.log('user reconnected with id', id);
+    
     if (!database.sessions[id]){
       establishSession();
+
     } else {
-      database.sessions[id].lastConnected = new Date().toISOString();
+      session = database.sessions.find( s => s.id == id );
+      session.lastConnected = new Date().getTime();
+
     }
+
+    sockets.push({
+      sock: socket,
+      session: session
+    });
+
+    session.status = 'CONNECTED';
   })
 
   socket.on('username', username => {
-    database.sessions[id].username = username;
+    session.username = username;
   });
   
   socket.on('disconnect', function(){
     console.log('user disconnected');
+    session.status = 'DISCONNECTED';
   });
   
   
 });
 
 var commitDatabase = function(){
-  fs.writeFileSync('./database.json',JSON.stringify(database));
+  let tmp = {
+    "users": database.users,
+    "sessions": database.sessions
+  }
+  fs.writeFileSync('./database.json',JSON.stringify(tmp));
 }
 
 http.listen(3000, function(){
-  sessions = JSON.parse(fs.readFileSync('./database.json'));
+
+  try {
+    database = JSON.parse(fs.readFileSync('./database.json'));
+    database.sessions.forEach( s => s.status = 'DISCONNECTED' );
+
+  } catch (e){
+    database = {
+      sessions: [],
+      users: []
+    };
+  }
+
   console.log('listening on *:3000');
 
   setInterval(commitDatabase,1000);
